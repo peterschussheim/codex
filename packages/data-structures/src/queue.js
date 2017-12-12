febcc95ec22f03363280da8dfba3ce00dea3a7e9
Peter Schussheim <peter@schussheim.com>
2017-12-12 16:56:02 -0500

    add queue class

---
+++ packages/data-structures/src/queue.js
@@ -0,0 +1,31 @@
+export default class Queue {
+  constructor() {
+    this._elements = new LinkedList()
+
+    Object.defineProperty(this, 'length', { get: () => this._elements.length })
+  }
+
+  isEmpty() {
+    return this._elements.isEmpty()
+  }
+
+  push(e) {
+    return this._elements.add(e)
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      throw new Error('Empty queue')
+    }
+    const e = this._elements.head
+    this._elements.delNode(e)
+    return e.value
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      throw new Error('Empty queue')
+    }
+    return this_._elements.get(0)
+  }
+}
