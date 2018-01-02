784605e2ebb75a2ec1277f7b2847591dde6f4565
Peter Schussheim <peter@schussheim.com>
2018-01-02 12:37:33 -0500

    fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,43 @@
-// TODO: update each package's 'name' key to match below
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+
 export const globals = {
   'codex-algorithms': 'codex.algorithms',
   'codex-data-structures': 'codex.dataStructures',
-  'codex-utilities': 'codex.utilities'
+  'codex-utils': 'codex.utils'
 }
 
 export default name => ({
-  input: 'lib/index.js',
-  output: {
-    file: 'lib/bundle.umd.js',
-    format: 'umd',
-    name: `codex.${name}`,
-    globals,
-    sourcemap: true,
-    exports: 'named'
-  },
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'lib/bundle.umd.js',
+      format: 'umd',
+      name: `codex.${name}`,
+      globals,
+      exports: 'named'
+    },
+    {
+      file: 'lib/index.js',
+      format: 'cjs',
+      name: `codex.${name}`,
+      globals,
+      exports: 'named'
+    },
+    {
+      file: 'lib/index.es.js',
+      format: 'es',
+      name: `codex.${name}`,
+      globals,
+      exports: 'named'
+    }
+  ],
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    commonjs()
+  ],
   external: Object.keys(globals),
   onwarn
 })
@@ -26,38 +49,3 @@ export function onwarn(message) {
     return console.warn(message.message)
   }
 }
-
-// import resolve from 'rollup-plugin-node-resolve'
-// import commonjs from 'rollup-plugin-commonjs'
-// import pkg from './package.json'
-
-// export default [
-//   // browser-friendly UMD build
-//   {
-//     input: 'src/main.js',
-//     output: {
-//       file: pkg.browser,
-//       format: 'umd'
-//     },
-//     name: 'howLongUntilLunch',
-//     plugins: [
-//       resolve(), // so Rollup can find `ms`
-//       commonjs() // so Rollup can convert `ms` to an ES module
-//     ]
-//   },
-
-//   // CommonJS (for Node) and ES module (for bundlers) build.
-//   // (We could have three entries in the configuration array
-//   // instead of two, but it's quicker to generate multiple
-//   // builds from a single configuration where possible, using
-//   // an array for the `output` option, where we can specify
-//   // `file` and `format` for each target)
-//   {
-//     input: 'src/main.js',
-//     external: ['ms'],
-//     output: [
-//       { file: pkg.main, format: 'cjs' },
-//       { file: pkg.module, format: 'es' }
-//     ]
-//   }
-// ]
